838fa6fce0ea3495b04ba1262a108b2329991ff0
ohuton55 <tttt.tttt.no@gmail.com>
2025-03-16 21:31:47 +0900

    basic parts

--- test.c
+++ test.c
@@ -2,9 +2,83 @@
 
 int main(void)
 {
+/*
+    multi comment
+    multi comment
+    multi comment
+*/
+    // print 3 lines
     printf("Hello, World\n");
-    printf("Line 2!\n");
-    printf("Line 3!\n");
+
+    // int speed = 0;
+    // int time = 0;
+    // printf("Speed: ");
+    // scanf("%d", &speed);
+    // printf("Time: ");
+    // scanf("%d", &time);
+
+    // int distance = speed * time;
+
+    // printf("dist: %d\n", distance);
+
+    // float x = 5.2;
+    // double y = -9.8;
+
+    // printf("x: ");
+    // scanf("%f", &x);
+    // printf("x: %f\n", x);
+
+    // printf("y: ");
+    // scanf("%lf", &y);
+    // printf("y: %f\n", y);
+
+    // char c = 'd';
+    // printf("c: ");
+    // scanf("%c", &c);
+    // printf("c: %c\n", c);
+    // printf("c: %d\n", c);
     
+    // double x = 5.2, y = 2.5;
+    // double mult = x * y;
+    // double add = x + y;
+    // double div  = x / y;
+    // double sub = x - y;
+
+    // printf("mult: %f\n", mult);
+    // printf("add: %f\n", add);
+    // printf("div: %f\n", div);
+    // printf("sub: %f\n", sub);
+
+    // int div = x / y;
+    // printf("div: %d\n", div);
+    // int x = 14, y = 3;
+    // int mod = x % 2;
+    // printf("mod: %d\n", mod);
+
+    // int grade = 0;
+    // printf("Grade: ");
+    // scanf("%d", &grade);
+
+    // if (grade >= 90)
+    // {
+    //     printf("A\n");
+    // }
+    // else if (grade >= 80) printf("B\n");
+    // else if (grade >= 70) printf("C\n");
+    // else if (grade >= 60)
+    // {
+    //     printf("D\n");
+    // }
+    // else printf("Fail\n");
+
+    int height = 0;
+    printf("height: ");
+    scanf("%d", &height);
+
+    if (height == 160)
+        printf("Height is 160!\n");
+    else
+        printf("Height is not 160!\n");
+
     return 0;
 }
