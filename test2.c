3a1e6a7e6c0607ea637ddf42b416ad03df4a8674
ohuton55 <tttt.tttt.no@gmail.com>
2025-03-17 21:17:43 +0900

    point rotation like this

--- test2.c
+++ test2.c
@@ -1,25 +1,69 @@
 #include<stdio.h>
 
+void add_one(int *a);
+void add_one_trio(int *a, int *b, int *c);
+void add_one_array(int array[], int length);
+void add_one_array_star(int *array, int length);
+
 int main(void)
 {
-    int a = 5;
-    int b = 10;
-    int *p;
-    p = &b;
-    printf(" p: %p\n", p);
+    int b = 5;
     printf("&b: %p\n", &b);
-    printf("&a: %p\n", &a);
-    printf("*p:%d\n", *p);
-    
-    
-    a = a + *p;
-    printf(" a: %d\n", a);
-    
-    p = &a;
-    printf(" p:%d\n", p);
-    *p = *p + 1;
-    printf("&a:%d\n", &a);
-    printf(" a:%d\n", a);
+    add_one(&b);
+    printf("b: %d\n", b);
 
+    int x1 = 1, x2 = 2, x3 = 3;
+    add_one_trio(&x1, &x2, &x3);
+    printf("x1: %d\n", x1);
+    printf("x2: %d\n", x2);
+    printf("x3: %d\n", x3);
+
+    int a[] = {1,2,3};
+    add_one_array(a, 3);
+    for (int i = 0; i < 3; i++)
+        printf("a[%d]=%d\n", i, a[i]);
+        
+    add_one_array_star(a, 3);
+    for (int i = 0; i < 3; i++)
+        printf("a[%d]=%d\n", i, a[i]);
+
+    int *p;
+    p = a;
+    printf("p: %p\n", p);
+    printf("a: %p\n", a);
+
+    printf("p[2] = %d\n", p[2]);
+    printf("a[2] = %d\n", a[2]);
+
+    // point rotation like this
+    printf("*p = %d\n", *p);
+    printf("*(p + 1) = %d\n", *(p + 1));
+    printf("*(p + 2) = %d\n", *(p + 2));
+    printf("*(a + 1) = %d\n", *(a + 1));
+    printf("*(a + 2) = %d\n", *(a + 2));
+
+    
     return 0;
+}
+
+void add_one(int *a)
+{
+    printf(" a: %p\n", a);
+    *a = *a + 1;
+}
+void add_one_trio(int *a, int *b, int *c)
+{
+    *a = *a + 1;
+    *b = *b + 1;
+    *c = *c + 1;
+}
+void add_one_array(int array[], int length)
+{
+    for (int i = 0; i < length; i++)
+        array[i] += 1;
+}
+void add_one_array_star(int *array, int length)
+{
+    for (int i = 0; i < length; i++)
+        array[i] += 1;
 }
