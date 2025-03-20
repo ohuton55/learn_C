2a89975c7df5afb3505726c2eb8fc22f54643938
ohuton55 <tttt.tttt.no@gmail.com>
2025-03-20 20:25:49 +0900

    file open & write

--- test2.c
+++ test2.c
@@ -2,14 +2,24 @@
 
 int main(int argc, char *argv[])
 {
-    // (type) expression
-    // (bouble) a
-    int a = 5;
-    int b = 2;  // b id upgraded to double
-    double c = (double)a / b;   // conversion first a
-    //double c = (double)(a / b);   // conversion after a / b
+    FILE *fh_write; // file handle variable
 
-    printf("%d / %d = %d\n", a, b, a / b);
-    printf("%d / %d = %f\n", a, b, c);
+     // w:open write.txt with write mode
+     // a:open write.txt with add mode
+     // return file handle
+    fh_write = fopen("write.txt", "a");
+    if (fh_write == NULL)
+    {
+        printf("Error opening FILE!");
+        return 1;
+    }
+    else
+    {
+        for (int i = 0; i < 10; i++)
+            // print on file handler
+            fprintf(fh_write, "%d\n", (i+1));
+        fclose(fh_write);
+    }
+
     return 0;
 }
