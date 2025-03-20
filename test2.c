7cd19e18099a10f0797c29ed5613f50b6f306f50
ohuton55 <tttt.tttt.no@gmail.com>
2025-03-20 20:50:17 +0900

    global scope variables

--- test2.c
+++ test2.c
@@ -1,41 +1,32 @@
 #include<stdio.h>
 #include<stdlib.h>
 
-#define LINE_BUFFER 100
-#define NUM_BUFFER 100
+#define NAME 5
+
+int y = 10; // global valuable cause hard understand
+
+void func();
 
 int main(int argc, char *argv[])
 {
-    FILE *fh_read;
-    fh_read = fopen("write.txt", "r");
+    int y = 2;
+    printf("y: %d\n", y);
+    printf("%d\n", NAME);
+    
+    // I can't assignment again this value
+    const int x = 5;
+    y++;
+    
+    printf("%d\n", x + 2);
+    func();
 
-    // if we can't open it return null
-    if (fh_read == NULL)
-    {
-        printf("file could not be opend\n");
-        return 1;
-    }
-    else
-    {
-        char buffer[LINE_BUFFER];   // store each file contents to buffer
-        int data[NUM_BUFFER];
-        int i = 0;
-        while (fgets(buffer, 100, fh_read) != NULL)
-        {
-            printf("%s\n", buffer);
-            data[i] = atoi(buffer);
-            i++;
-        }    // count of 100
-        fclose(fh_read);
-
-        int total = 0;
-        int num_lines = i;
-        for (int j = 0; j < num_lines; j++)
-        {
-            printf("data[%d] = %d\n", j, data[j]);
-            total += data[j];
-        }
-        printf("avg: %d\n", total / num_lines);
-    }
     return 0;
+}
+
+void func()
+{
+    // const value can't access here
+    printf("%d\n", NAME);
+    y++;
+    printf("y: %d\n", y);
 }
