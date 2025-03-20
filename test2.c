8a5e02ea6303e4a01ed8cdd478bdf82f401a106e
ohuton55 <tttt.tttt.no@gmail.com>
2025-03-20 19:58:10 +0900

    use atoi()

--- test2.c
+++ test2.c
@@ -1,10 +1,22 @@
 #include<stdio.h>
+#include<stdlib.h>
 
 int main(int argc, char *argv[])
 {
-    printf("argc: %d\n", argc);
+    if (argc == 3)
+    {
+        int num_times = atoi(argv[2]);
     
-    for (int i = 0; i < argc; i++)
-        printf("argv[%d]=%s\n", i, argv[i]);
-    return 0;
+        for (int i = 0; i < num_times; i++)
+            printf("%s\n", argv[1]);
+
+    }
+    else
+    {
+        printf("2 argument except.\n");
+        printf("1st - string to print out\n");
+        printf("2st - # of times to print\n");
+        return 1;
+    }
+
 }
