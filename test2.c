6785e415a962b2a6c72c636b8700387150f9929f
ohuton55 <tttt.tttt.no@gmail.com>
2025-03-20 20:41:01 +0900

    constans with define

--- test2.c
+++ test2.c
@@ -1,25 +1,41 @@
 #include<stdio.h>
+#include<stdlib.h>
+
+#define LINE_BUFFER 100
+#define NUM_BUFFER 100
 
 int main(int argc, char *argv[])
 {
-    FILE *fh_write; // file handle variable
+    FILE *fh_read;
+    fh_read = fopen("write.txt", "r");
 
-     // w:open write.txt with write mode
-     // a:open write.txt with add mode
-     // return file handle
-    fh_write = fopen("write.txt", "a");
-    if (fh_write == NULL)
+    // if we can't open it return null
+    if (fh_read == NULL)
     {
-        printf("Error opening FILE!");
+        printf("file could not be opend\n");
         return 1;
     }
     else
     {
-        for (int i = 0; i < 10; i++)
-            // print on file handler
-            fprintf(fh_write, "%d\n", (i+1));
-        fclose(fh_write);
-    }
+        char buffer[LINE_BUFFER];   // store each file contents to buffer
+        int data[NUM_BUFFER];
+        int i = 0;
+        while (fgets(buffer, 100, fh_read) != NULL)
+        {
+            printf("%s\n", buffer);
+            data[i] = atoi(buffer);
+            i++;
+        }    // count of 100
+        fclose(fh_read);
 
+        int total = 0;
+        int num_lines = i;
+        for (int j = 0; j < num_lines; j++)
+        {
+            printf("data[%d] = %d\n", j, data[j]);
+            total += data[j];
+        }
+        printf("avg: %d\n", total / num_lines);
+    }
     return 0;
 }
